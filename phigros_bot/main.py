0795d5ac11df7d7ad5b7673cc1ebc1e5f0b8f005
purofle <purofle@gmail.com>
2023-03-18 22:36:13 +0800

    closes: #10, #11

--- phigros_bot/main.py
+++ phigros_bot/main.py
@@ -5,14 +5,14 @@ Copyright 2022-2023 Purofle and contributors.
 Use of this source code is governed by the GNU AGPLv3 license that can be found through the following link.
 
 https://www.gnu.org/licenses/gpl-3.0.html
-
-运行需要传入 Bot Token 作为参数
 """
 import hashlib
 import json
 import logging
+import random
+import itertools
 import sys
-from typing import Any, Dict
+from typing import Any, Dict, List
 
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import InlineQuery, InputTextMessageContent, InlineQueryResultArticle
@@ -27,16 +27,66 @@ dp = Dispatcher(bot)
 
 logging.info("读取 JSON 信息")
 with open("phigros_bot/Phigros.json", "r") as f:
-    raw_json = f.read()
+    raw_phigros_json = f.read()
 
-phigros: Dict[str, Any] = json.loads(raw_json)
+phigros: Dict[str, Any] = json.loads(raw_phigros_json)
 music_name = list(phigros.keys())
-logging.info(f"JSON 信息加载完成，大小为{len(phigros)}")
+logging.info(f"谱面 JSON 信息加载完成，大小为{len(phigros)}")
+
+with open("phigros_bot/tips.json", "r") as f:
+    raw_tips_json = f.read()
+
+raw_tips: Dict[str, List[str]] = json.loads(raw_tips_json)
+
+# 将 raw_tips 展开为 List[str]
+tips = list(itertools.chain.from_iterable(raw_tips.values()))
+
+logging.info(f"Tips JSON 信息加载完成，大小为{len(tips)}")
+
+info = {
+        "歌名": "song",
+        "曲绘": "illustration",
+        "高清曲绘": "illustration_big",
+        "BPM": "bpm",
+        "曲师": "composer",
+        "长度": "length",
+        "画师": "illustrator",
+    }
+
+chart_info = {
+        "等级": "level",
+        "定数": "difficulty",
+        "Max Combo": "combo",
+        "谱师": "charter",
+}
 
 
 @dp.message_handler(commands=["start", "help"])
 async def send_welcome(message: types.Message):
     await message.reply("Hi!\n欢迎使用该 Bot! 请使用 inline 调用。\n")
+
+
+@dp.message_handler(commands=["random"])
+async def send_random(message: types.Message):
+    random_song = random.sample(tuple(phigros.values()), 1)[0]
+
+    basic_info = "\n".join([f"{i[0]}: {random_song.get(i[1])}" for i in info.items()])
+    charts: Dict[str, Dict[str, str]] = random_song["chart"]
+    chart_basic_info = ""
+    for i in charts.keys():
+        chart_basic_info += i + "\n"
+        chart_basic_info += "\n".join(
+            [f"{c[0]}: {charts[i].get(c[1])}" for c in chart_info.items()]
+        )
+        chart_basic_info += "\n\n"
+
+    await message.reply(basic_info + "\n\n" + chart_basic_info.strip())
+
+
+@dp.message_handler(commands=["tip"])
+async def get_tip(message: types.Message):
+    tip = random.sample(tips, 1)[0]
+    await message.reply(tip)
 
 
 @dp.inline_handler()
@@ -63,23 +113,6 @@ async def find_music(inline_query: InlineQuery):
         )
         await bot.answer_inline_query(inline_query.id, results=[item], cache_time=1)
         return
-
-    info = {
-        "歌名": "song",
-        "曲绘": "illustration",
-        "高清曲绘": "illustration_big",
-        "BPM": "bpm",
-        "曲师": "composer",
-        "长度": "length",
-        "画师": "illustrator",
-    }
-
-    chart_info = {
-        "等级": "level",
-        "定数": "difficulty",
-        "Max Combo": "combo",
-        "谱师": "charter",
-    }
 
     basic_info = "\n".join([f"{i[0]}: {music_info.get(i[1])}" for i in info.items()])
     charts: Dict[str, Dict[str, str]] = music_info["chart"]
